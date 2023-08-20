becf798f6505cae3db9450bcbc8443567f65a3fa
wirelesshobo <4298567+wirelesshobo@users.noreply.github.com>
2023-08-20 12:26:49 -0500

    Cleaned up unnecessary Try/Catch statements, provided more comments, reorganized operations to follow AD, AAD, Intune lookup order of processing.

--- Check-IntuneEnrollment.ps1
+++ Check-IntuneEnrollment.ps1
@@ -17,7 +17,9 @@
   Link:           https://wirelesshobo.com
 #>
 
-
+# Command line parameters
+# OUSearchBase provides a search option for local Active Directory
+# OutputPath provides a location for the report output
 param(
     [Parameter(
         Mandatory = $true,
@@ -35,31 +37,46 @@ param(
 # Define Action to take on Error
 $ErrorActionPreference = "Stop"
 
+# Capture current UI foreground color
+$originalColor = $host.ui.RawUI.ForegroundColor
+
+try {
+    Resolve-Path -Path $OutputPath -ErrorAction Stop
+}
+catch {
+    Write-Warning "OutputPath does not exist.  Please create the path before running this report again."
+    exit 0;
+}
+
+
+# Try to import each of the needed PowerShell Modules and attempt helpful advice on how to resolve if missing.
 try {
     Import-Module ActiveDirectory
 }
 catch {
-    Write-Host "Error loading ActiveDirectory module. Look into adding the module through optional feature Rsat.ActiveDirectory.DS-LDS.Tools."
+    Write-Warning "Error loading ActiveDirectory module. Look into adding the module through optional feature Rsat.ActiveDirectory.DS-LDS.Tools."
+    exit 0;
 }
 
 try{
 Import-Module AzureAD;
 }
 catch {
-    Write-Host "Error loading AzureAD module.  Consider install-module AzureAD."
+    Write-Warning "Error loading AzureAD module.  Consider install-module AzureAD."
+    exit 0;
 }
 
 try{
     Import-Module Microsoft.Graph.Intune 
-} catch {
-    Write-Host "Error loading Microsoft.Graph.Intune module.  Consider install-module Microsoft.Graph.Intune."
+} 
+catch {
+    Write-Warning "Error loading Microsoft.Graph.Intune module.  Consider install-module Microsoft.Graph.Intune."
+    exit 0;
 }
 
 
 
-
-
-# Connect to MSGraph
+# Attempt to connect with Intune MSGraph
 try {
     Connect-MSGraph -ErrorAction Stop;
     Write-Output "Connected to Intune."
@@ -69,7 +86,7 @@ try {
     Write-Output $message
 }
 
-# Connect to AzureAD
+# Attempt to connect with AzureAD
 try {
     Connect-AzureAD -ErrorAction Stop;
     Write-Output "Connected to AAD."
@@ -82,26 +99,28 @@ try {
 # AllWorkstations array initialization.
 $AllWorkstations = @();
 
-# Output file with timestamp.
+# Generate a timestamp that should guarantee a unique filename.
 $timestamp = get-date -Format yyyyMMddhhmmss
-$filename = $outputpath + "\HybridAzureADIntuneDeviceReport_" + $timestamp + ".csv"
 
-# ADWorkstations is an array of all computer objects under the Workstations OU
+# Generate the filename from the OutputPath, report name, and timestamp.
+$filename = $outputpath + "\HAADJIntuneDeviceReport_" + $timestamp + ".csv"
+
+# Get all devices listed in Active Directory's OUSearchBase passed in by parameter
 $ADWorkstations = get-adcomputer -SearchBase $OUSearchBase -filter * -Properties LastLogonDate
 Write-Output "Getting a list of computer objects from $($OUSearchBase).  $($ADWorkstations.count) objects found."
 
-# AzureDevices
+# Get all devices listed in Azure Active Directory
 $AzureADDevices = Get-AzureADDevice -All $true
 Write-Output "Getting a list of all AzureAD Devices. $($AzureADDevices.count) devices found."
 
-# IntuneDevices
+# Get all devices enrolled in Intune
 $IntuneManagedDevices = Get-IntuneManagedDevice
 Write-Output "Getting a list of all Intune Managed Devices. $($IntuneManagedDevices.count) devices found."
 
 # Initialize a number count for progress bar.
 $i=0;
 
-# Create a count of total workstations to process.
+# Create a count of total workstations to process for the progress bar.
 $ADWorkstationTotal = $ADWorkstations.count;
 
 # Initialize and Output Progress Bar
@@ -111,60 +130,43 @@ $Progress = @{
     Status = 'Searching AzureAD and Intune'
     PercentComplete = 0
 }
-
 Write-Progress @Progress;
 
-# For each workstation in AD check for enrollment in Intune
+# For each workstation in AD check for Azure Active Directory registration and Intune enrollment
 foreach ($workstation in $ADWorkstations) {
 
+    # Increment the I variable.
     $i++;
+    # Generate a rounded percentage of completed devices processed
     $roundedPercentageCompleted = [math]::Round((($i / $ADWorkstationTotal) * 100), [System.MidpointRounding]::AwayFromZero);
     
-
-    #Assign device name to be the workstation.name
+    # Assign more friendly variable name to Active Directory attributes name and ObjectGUID
     $ADDeviceName = $workstation.name;
     $ADObjectGUID = $workstation.ObjectGUID; 
 
-    #Write-Output "Computer Objects Processed: $($numbercount)  Percent Complete: $($Completed)"
-
+    
+    # Output the progress bar while we are performing lookups from AD workstations against AAD and Intune arrays
     $Progress.CurrentOperation = $ADDeviceName 
     $Progress.Status = 'Searching AzureAD and Intune: '
     $Progress.PercentComplete = $roundedPercentageCompleted
     Write-Progress @Progress
     Start-Sleep -Milliseconds 20
 
-	try {
 
-		# Try to obtain the enrolled Intune device based on the computer object device name
-		$IntuneDevice = $IntuneManagedDevices | Where-Object {$_.azureADDeviceId -eq $ADObjectGUID};
-        
+    # All AD devices share a guid with the three other systems.  This allows our lookups to be name agnostic.
+    # Active Directory is ObjectGUID
+    # AzureAD is DeviceID
+    # Intune is AzureADDeviceID
 
-	}  catch { 
+    # Obtain the Azure AD Device from AzureADDevices with a matching AD objectGUID
+    $AzureADDevice = $AzureADDevices | Where-Object {$_.deviceid -eq $ADObjectGUID}
 
-        # An error occurred accessing Intune in some way.
-        $message = $_.Exception.$message
-		Write-Output "An error occurred with obtaining data from Intune.";
-        Write-Output $message
+    # Obtain the Intune enrolled device from IntuneManagedDevices that match the AD objectGUID
+	$IntuneDevice = $IntuneManagedDevices | Where-Object {$_.azureADDeviceId -eq $ADObjectGUID};
+    
+    # Now for each AD device, determine its AAD registration and Intune enrollment status and create a new device object.
 
-	}
-
-
-    try {
-
-         #Try to obtain Azure AD data on device.
-         $AzureADDevice = $AzureADDevices | Where-Object {$_.deviceid -eq $ADObjectGUID}
-
-    } catch {
-
-        # An error occurred accessing Azure AD in some way.
-        $message = $_.Exception.$message
-        Write-Output "An error occurred with obtaining data from AzureAD."
-        Write-Output $message
-
-
-    }
-
-	# If the IntuneDevice result is NULL generate a not found object, else generate the device object with full details.
+	# If the IntuneDevice is not enrolled and the AzureADDevice is not registered, we probably need to troubleshoot why it is not hybrid registered.
 	if ( ($IntuneDevice -eq $NULL) -and ($AzureADDevice -eq $NULL) ) {
         #Create device object showing not enrolled for last check in time.
     		$tempDeviceObject = [PSCustomObject]@{
@@ -185,10 +187,9 @@ foreach ($workstation in $ADWorkstations) {
         # Add device object to AllWorkstations 
         $AllWorkstations += $tempDeviceObject;
     }  
-
+    # If the IntuneDevice is enrolled and AzureADDevice is not registered -- this should probably never happen.
     elseif ( ($IntuneDevice -ne $NULL) -and ( $AzureADDevice -eq $NULL ))  {
-        #Create device object enrolled in Intune, but not returning value in AzureAD.
-        $tempDeviceObject = [PSCustomObject]@{
+            $tempDeviceObject = [PSCustomObject]@{
             ADName = $workstation.name
             ADLastLogonDate = $workstation.LastLogonDate
             ADEnabled = $workstation.Enabled
@@ -206,10 +207,10 @@ foreach ($workstation in $ADWorkstations) {
         # Add device object to AllWorkstations 
         $AllWorkstations += $tempDeviceObject;
 	}
-
+    # If the Intune device is not enrolled, but is registered in Azure AD, troubleshoot the Intune enrollment process.
     elseif ( ($IntuneDevice -eq $NULL) -and ( $AzureADDevice -ne $NULL ))  {
-        #Create device object not enrolled in Intune, but registered in AzureAD.
-        $tempDeviceObject = [PSCustomObject]@{
+            #Create device object not enrolled in Intune, but registered in AzureAD.
+            $tempDeviceObject = [PSCustomObject]@{
             ADName = $workstation.name
             ADLastLogonDate = $workstation.LastLogonDate
             ADEnabled = $workstation.Enabled
@@ -227,10 +228,9 @@ foreach ($workstation in $ADWorkstations) {
         # Add device object to AllWorkstations 
         $AllWorkstations += $tempDeviceObject;
 	}
-
+    # If the Intune device is enrolled and Azure AD registered, we likely have a healthy enrollment.
     else {
-        #Create device object returning values in AzureAD and Intune.
-        $tempDeviceObject = [PSCustomObject]@{
+            $tempDeviceObject = [PSCustomObject]@{
             ADName = $workstation.name
             ADLastLogonDate = $workstation.LastLogonDate
             ADEnabled = $workstation.Enabled
@@ -251,6 +251,9 @@ foreach ($workstation in $ADWorkstations) {
     }
 }
 
-# Output all workstations to CSV for formatting.
+# Output all workstations to CSV filename for further formatting
+$AllWorkstationsCount = $AllWorkstations.count
 $AllWorkstations | export-csv $filename -NoTypeInformation;
-Write-Output "Writing CSV to " + $filename
+$host.ui.RawUI.ForegroundColor = "DarkGreen"
+Write-Output "Writing $($AllWorkstations.Count) devices to $filename."
+$host.ui.RawUI.ForegroundColor = $originalColor
